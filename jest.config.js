f38800d002b58198e199e26afcce0a749b771d93
Morris Kurz <morriskurz@gmail.com>
2024-07-11 14:22:30 +0200

    Fix jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,18 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js',
-    'jsx',
-    'mts',
-    'cts',
-    'mjs',
-    'cjs',
-  ],
-  testMatch: ['**/*.test.mts'],
+  resolver: "<rootDir>/mjs-resolver.ts",
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.(ts|tsx|mts|mjs)?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+    "^.+\\.(js|jsx|mjs|cjs)$": "babel-jest",
+  },
+  transformIgnorePatterns: ["node_modules"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "mts", "cts", "mjs", "cjs"],
+  testMatch: ["**/*.test.mts", "**/*.test.ts"],
 };
--- src/index.test.mts
+++ src/index.test.mts
@@ -2,5 +2,6 @@ describe('insert a test group description', () => {
     it('insert a test description', async () => {
         const response = await fetch('http://localhost:3000/');
         // TODO: 
+        expect(response).toEqual(null);
     });
 });
